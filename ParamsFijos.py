eef2bcf9a7c5aa30e31994a55d1524f7e93f9fd6
JoacoVial <64756805+JoacoVial@users.noreply.github.com>
2022-11-30 16:53:50 -0300

    creacion

---
+++ ParamsFijos.py
@@ -0,0 +1,14 @@
+N_SIM =100
+
+MaxGen = 2000
+MaxGenHOF = 500
+
+file = 'definitivo-iteracion-3'
+
+training_data = [
+                    {'name': '../data/gsk80.csv', 'train':1, 'test':0},
+                    {'name': '../data/gsk85.csv', 'train':0, 'test':1},
+                    {'name': '../data/gsk90.csv', 'train':1, 'test':0},
+                    {'name': '../data/gsk95.csv', 'train':0, 'test':1},
+                    {'name': '../data/gsk100.csv', 'train':1, 'test':0}
+]
